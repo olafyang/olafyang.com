278b81fb4c12427e53a3a0996528bf64ad01e210
Olaf Yang <44876184+olafyang@users.noreply.github.com>
2025-04-14 20:03:47 +0200

    moved to astro

--- nuxt/layouts/noNav.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div id="app">
-    <div class="fullpage-view">
-      <slot />
-    </div>
-  </div>
-</template>
--- nuxt/utils/objectId.ts
+++
@@ -1,9 +0,0 @@
-export function toURLSafe(id: string) {
-  // replaces . with _
-  return id.replace(/\./g, "_");
-}
-
-export function fromURLSafe(id: string) {
-  // replaces _ with .
-  return id.replace(/_/g, ".");
-}
