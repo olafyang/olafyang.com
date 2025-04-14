89f10b524e379621e97e32749c3dbb2330d08a89
Olaf Yang <44876184+olafyang@users.noreply.github.com>
2025-04-14 19:23:17 +0200

    rewrite to astro

---
+++ astro/src/composables/provideKeys.ts
@@ -0,0 +1,8 @@
+// import { type InjectionKey } from "vue";
+// import { type SanityClient } from "@sanity/client";
+// import builder from "@sanity/image-url";
+
+// export const sanityClientKey: InjectionKey<SanityClient> =
+//   Symbol("sanityClient");
+// export const urlBuilderKey: InjectionKey<ReturnType<typeof builder>> =
+//   Symbol("imageUrl");
